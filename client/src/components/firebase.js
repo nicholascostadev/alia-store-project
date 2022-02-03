72f6a6a783a7cbfb995333f58076917d03c897d5
Nicholas <nicholascostadev@gmail.com>
2022-02-03 20:10:42 -0300

    Solve responsiveness problem with Contact component

--- client/src/components/firebase.js
+++ client/src/components/firebase.js
@@ -1,5 +1,7 @@
 import firebase from 'firebase';
+
 const firebaseApp = firebase.initializeApp({
+	
 	apiKey: process.env.REACT_APP_API_KEY,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
 	databaseURL: process.env.REACT_APP_DATABASE_URL,
